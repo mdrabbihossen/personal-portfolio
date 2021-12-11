0c8cecc8914bb4ecbf7021ea6e12997e822a8111
Rabby-Cyber <ariyanrabby94@gmail.com>
2021-12-11 22:45:24 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,28 +1,36 @@
 import React from "react";
-import {
-  FaFacebook,
-  TiSocialLinkedinCircular,
-  AiFillGithub,
-} from "react-icons/fa";
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer text-center py-1 py-md-3 ">
       <div className="social-icon py-3 py-md-4">
-        <a href="https://web.facebook.com/noman.apparel.4003/" className="mr-3">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.facebook.com/fazleybinmahbub26"
+          className="mr-3"
+        >
           <FaFacebook />
         </a>
         <a
-          href="https://www.linkedin.com/in/md-abdulla-al-noman-0701171ba/"
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/fazleybinmahbub26/"
           className="mr-3"
         >
-          <TiSocialLinkedinCircular />
+          <FaLinkedin />
         </a>
-        <a href="https://github.com/mdabdullaalnoman" className="mr-3">
-          <AiFillGithub />
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/FazleyBin-Mahbub"
+          className="mr-3"
+        >
+          <FaGithub />
         </a>
       </div>
-      <p> All rised reserved &copy; MD Rabbi Hossen @2021</p>
+      <p> &copy; All rised reserved @MD Rabbi Hossen 2021</p>
     </div>
   );
 };
